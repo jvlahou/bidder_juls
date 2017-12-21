df4fae7b3aa0342a9a68844d2019e7bd67f7ca5f
juls <jvlahou@gmail.com>
2017-12-21 18:10:45 +0200

    Correction in npm modules installation flag

--- install_modules.sh
+++ install_modules.sh
@@ -3,19 +3,19 @@
 apt-get install mocha
 npm init
 #dev
-npm install express --save-dev
-npm install request --save-dev
+npm install express --save
+npm install request --save
 npm install promise-any
-npm install request-promise --save-dev
-npm install async --save-dev
+npm install request-promise --save
+npm install async --save
 
 #test
-npm install chai --save-dev
-npm install chai-http --save-dev
-npm install babel-preset-env --save-dev
-npm install diff --save-dev
-npm install commander --save-dev
-npm install glob --save-dev
+npm install chai --save
+npm install chai-http --save
+npm install babel-preset-env --save
+npm install diff --save
+npm install commander --save
+npm install glob --save
 ln -s /usr/bin/nodejs /usr/bin/node
 touch test/test_response_bid.js
 #export NODE_PATH=/home/julie/Desktop/bidder_juls/node_modules 
